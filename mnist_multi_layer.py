bc2c73c5f6dc0a816cc60fa3b3454951d996ebb5
benji <benji.b.ramirez@gmail.com>
2017-06-01 22:20:41 -0700

    created a cifar-10 cnn model

--- mnist_multi_layer.py
+++ mnist_multi_layer.py
@@ -44,5 +44,5 @@ model = multi_layer_model()
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
-print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+print("Multi-Layer NN Error: %.2f%%" % (100-scores[1]*100))
 
