b1fd4f5e452933970c6d117be17024d128896393
benji <benji.b.ramirez@gmail.com>
2017-06-01 17:43:29 -0700

    init commit, base implementation of cnn, singl-layer, multi-layer for MNIST

---
+++ mnist_single_layer.py
@@ -0,0 +1,43 @@
+# Benjamin Ramirez Jun 1, 2017
+# Modified Code from cnn_file to test performance of single layer Deep NN vs Convolutional
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# reshape to be [samples][width * height] = [60000][28*28]
+X_train = X_train.reshape(X_train.shape[0], 784).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 784).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+def single_layer_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(512, input_shape=(784,), activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_classes, input_shape=(784,), activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+# build the model
+model = single_layer_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+
